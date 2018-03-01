d6436e14bd87991ed3fc7dda12c525f51eb8542e
yuriy-a76 <yuriyakishin@list.ru>
2018-03-02 01:22:27 +0200

    3.6.2

---
+++ app/code/Unit3/Test/Block/Template.php
@@ -0,0 +1,6 @@
+<?php
+namespace Unit3\Test\Block;
+
+class Template extends \Magento\Framework\View\Element\Template
+{
+}
---
+++ app/code/Unit3/Test/Controller/Action/Index.php
@@ -0,0 +1,12 @@
+<?php
+namespace Unit3\Test\Controller\Action;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        $block = $this->_view->getLayout()->createBlock(\Unit3\Test\Block\Template::class);
+        $block->setTemplate('Unit3_Test::test.phtml');
+        $this->getResponse()->appendBody($block->toHtml());
+    }
+}
