047d531e0cc39299be3a4df7a7afa74868d79c19
yuriy-a76 <yuriyakishin@list.ru>
2018-03-02 18:02:35 +0200

    3.8.1.

---
+++ app/code/Unit3/Render/Block/Template.php
@@ -0,0 +1,6 @@
+<?php
+namespace Unit3\Render\Block;
+
+class Template extends \Magento\Framework\View\Element\Template
+{
+}
---
+++ app/code/Unit3/Render/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Unit3_Render',
+    __DIR__
+);
